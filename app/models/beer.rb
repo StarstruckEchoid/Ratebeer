f95fb2890ccd50ac6f4d3d221f714d9b9a7b882f
Juho Gröhn <juho.grohn@gmail.com>
2018-09-10 12:39:39 +0300

    Tehty tehtäviä T4 saakka.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,15 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        sum = 0.0
+        rs = self.ratings.all
+        rc = rs.count
+        return nil if (rc==0)
+        rs.each do |r|
+            sum+=r.score
+        end
+        sum/rc
+    end
 end
