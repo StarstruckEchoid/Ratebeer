7afb16bf93a95b7fdda0911780296a2ab4c1e57a
Juho Gröhn <juho.grohn@gmail.com>
2018-09-10 11:46:28 +0300

    Tehtiin viikon 2 harjoitukset Tehtävän 1 loppuun asti.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'breweries#index'
+  get 'ratings', to: 'ratings#index'
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
