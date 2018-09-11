1df95bcff3d0af8834a8ef89a4685ddb41cb468d
Juho Gröhn <juho.grohn@gmail.com>
2018-09-11 10:43:32 +0300

    Tehtiin tehtävät 7 ja 11.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'breweries#index'
-  resources :ratings, only: [:index, :new, :create]
+  resources :ratings, only: [:index, :new, :create, :destroy]
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
