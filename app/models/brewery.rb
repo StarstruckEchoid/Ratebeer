2c5f79ee8fe82898a0168c8540a708451cd21c56
Juho Gröhn <juho.grohn@gmail.com>
2018-09-17 17:09:54 +0300

    Tehtiin tehtävät 7 ja 8.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,6 +3,12 @@
 class Brewery < ApplicationRecord
   include RatingAverage
 
+  validates :name, presence: true
+
+  validates :year, numericality: { less_than_or_equal_to: :current_year,
+                                   greater_than_or_equal_to: 1040,
+                                   only_integer: true }
+
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
@@ -15,5 +21,11 @@ class Brewery < ApplicationRecord
   def restart
     self.year = 2018
     puts "changed year to #{year}"
+  end
+
+  private
+
+  def current_year
+    Time.new.year
   end
 end
