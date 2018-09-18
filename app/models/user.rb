77051af4f607f102e8385037c0882828378ec5de
Juho Gröhn <juho.grohn@gmail.com>
2018-09-18 13:24:32 +0300

    Toteutettiin salasana-autentikointi. Tehtiin tehtävä 12.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   include RatingAverage
 
+  has_secure_password
+
   validates :username, uniqueness: true,
                        length: { minimum: 3,
                                  maximum: 30 }
+
+  validates :password, length: { minimum: 4 },
+                       format: { with: /(?=.*\d+)(?=.*[A-Z]+)/, message: "Password must contain a number and an uppercase letter."}
 
   has_many :ratings
   has_many :beers, through: :ratings
