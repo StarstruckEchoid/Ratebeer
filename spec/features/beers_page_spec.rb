b9584fd1f0fe431b5ded53de52d37356c5be5678
Juho Gröhn <juho.grohn@gmail.com>
2018-09-26 10:45:01 +0300

    Täydennettiin tehtävään 5 epäonnistumistesti.

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -2,13 +2,28 @@ require 'rails_helper'
 
 describe "Beer" do
 
-  it "can be created on the beer creation page, if given name is non-empty, style is selected, and brewery is selected" do
-    brewery = FactoryBot.create(:brewery, name:'TestBrew')
+  describe "on the beer creation page, with a valid style and brewery" do
+    before :each do
+      initialise_style_and_brewery
+    end
 
-    visit new_beer_path
-    fill_in('beer_name', with:'Frosty Test Lager')
-    select('Lager', from:'beer_style')
-    select('TestBrew', from:'beer_brewery_id')
-    expect{ click_button "Create Beer" }.to change{Beer.count}.by(1)
+    it "can be created if given name is non-empty" do
+      fill_in('beer_name', with:'Frosty Test Lager')
+      expect{ click_button "Create Beer" }.to change{Beer.count}.by(1)
+    end
+
+    it "can't be created, if given name is empty" do
+      expect{ click_button "Create Beer" }.to change{Beer.count}.by(0)
+    end
   end
+end
+
+private
+
+def initialise_style_and_brewery
+  brewery = FactoryBot.create(:brewery, name:'TestBrew')
+
+  visit new_beer_path
+  select('Lager', from:'beer_style')
+  select('TestBrew', from:'beer_brewery_id')
 end
