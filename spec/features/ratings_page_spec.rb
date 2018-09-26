3cdfa332fcb7e5703d8952c219f8662b55ecff94
Juho Gröhn <juho.grohn@gmail.com>
2018-09-26 11:08:17 +0300

    Tehtiin tehtävä 6.

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -8,21 +8,41 @@ describe "Rating" do
   let!(:beer2) { FactoryBot.create :beer, name:"Karhu", brewery:brewery }
   let!(:user) { FactoryBot.create :user }
 
-  before :each do
-    sign_in(username:"Pekka", password:"Foobar1")
+  describe "Create" do
+    before :each do
+      sign_in(username:"Pekka", password:"Foobar1")
+    end
+
+    it "when given, rating is registered to the beer and user who is signed in" do
+      visit new_rating_path
+      select('iso 3', from:'rating_beer_id')
+      fill_in('rating_score', with:'15')
+
+      expect{
+        click_button "Create Rating"
+      }.to change{Rating.count}.from(0).to(1)
+
+      expect(user.ratings.count).to eq(1)
+      expect(beer1.ratings.count).to eq(1)
+      expect(beer1.average_rating).to eq(15.0)
+    end
   end
 
-  it "when given, is registered to the beer and user who is signed in" do
-    visit new_rating_path
-    select('iso 3', from:'rating[beer_id]')
-    fill_in('rating[score]', with:'15')
-
-    expect{
-      click_button "Create Rating"
-    }.to change{Rating.count}.from(0).to(1)
-
-    expect(user.ratings.count).to eq(1)
-    expect(beer1.ratings.count).to eq(1)
-    expect(beer1.average_rating).to eq(15.0)
+  describe "Index" do
+  
+    it "says there are no ratings when there are no ratings" do
+      visit ratings_path
+      expect(page).to have_content('0 ratings')
+    end
+  
+    it "shows rating count and the ratings, when there are ratings" do
+      FactoryBot.create(:rating, beer: beer1, user: user, score: 10)
+      visit ratings_path
+      expect(page).to have_content('1 rating')
+      expect(page).to have_content('iso 3')
+      expect(page).to have_content('10')
+      expect(page).to have_content('Pekka')
+    end
   end
 end
+
