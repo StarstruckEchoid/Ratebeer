722ffbe2fec1b16aba5787acf93f4d2fc3f2db07
Juho Gröhn <juho.grohn@gmail.com>
2018-10-01 17:59:03 +0300

    Tehtiin tehtävä 4.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.minute.to_i}"
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
@@ -44,5 +44,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.cache_story = :memory_store
+  config.cache_store = :memory_store
 end
