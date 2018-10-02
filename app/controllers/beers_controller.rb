b3cc76c752447f4d7a8856d46e7dd8ef32c76262
Juho Gröhn <juho.grohn@gmail.com>
2018-10-02 14:46:54 +0300

    Tehtiin tehtävä 7.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -12,11 +12,15 @@ class BeersController < ApplicationController
   # GET /beers/1
   # GET /beers/1.json
   def show
+    @rating = Rating.new
+    @rating.beer = @beer
   end
 
   # GET /beers/new
   def new
     @beer = Beer.new
+    @beer.brewery = Brewery.first
+    @beer.style = @styles.first
   end
 
   # GET /beers/1/edit
