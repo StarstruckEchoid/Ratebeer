8df9a0251a7c9f64dee1d50ecfeac9df203377d9
Juho Gröhn <juho.grohn@gmail.com>
2018-10-04 14:07:37 +0300

    Tehtiin tehtävän 11 style-migraatiot.

---
+++ db/migrate/20181004104320_add_style_from_old_style_to_beers.rb
@@ -0,0 +1,9 @@
+class AddStyleFromOldStyleToBeers < ActiveRecord::Migration[5.2]
+  def up
+    Beer.all.each { |b| b.update(style: Style.find_by(name: b.old_style)) }
+  end
+
+  def down
+    Beer.all.each { |b| b.update(old_style: b.style.name) }
+  end
+end
