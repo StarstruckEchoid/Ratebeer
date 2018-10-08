6e8bbe8a052c5e9d6519caa769e875fcc82a5051
Juho Gröhn <juho.grohn@gmail.com>
2018-10-08 12:26:17 +0300

    Tehtiin tehtävä 4. Kauniimpi signin-näkymä.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to :root
+    redirect_to :root, notice: "Sign out successful."
   end
 end
